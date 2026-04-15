091a67dbd1e3f39578c2fe247fef5dd3b3661176
Hamster <hamster@lonebytesoft.com>
2026-04-15 19:37:17 +0200

    chore: add basic project structure

---
+++ src/main/java/com/lonebytesoft/hamster/eventnotifybot/Test.java
@@ -0,0 +1,14 @@
+package com.lonebytesoft.hamster.eventnotifybot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+
+    private static final Logger log = LoggerFactory.getLogger(Test.class);
+
+    static void main(String[] args) {
+        log.info("Message");
+    }
+
+}
